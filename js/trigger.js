8406022716da188b3a37f801fd9d8a79184247cc
cyrilhamidechi <cyril@hamidechi.com>
2017-11-13 07:43:50 +0100

    Private events files support

--- js/trigger.js
+++ js/trigger.js
@@ -18,11 +18,18 @@ const contexts = [
 
 var event;
 var eventFilename = defaults.event;
+var eventFilepath;
 try {
   if(process.argv.length>2) {
     eventFilename = process.argv[2];
   }
-  event = require('./samples/' + eventFilename  + '.json');
+  var folder = "samples";
+  if("p-" == eventFilename.substr(0,2)) {
+    folder = "private";
+    eventFilename = eventFilename.substr(2);
+  }
+  eventFilepath = './' + folder  + '/' + eventFilename  + '.json';
+  event = require(eventFilepath);
 } catch (e) {
   console.log(e);
 }
@@ -33,6 +40,7 @@ if(process.argv.length>3 && contexts[parseInt(process.argv[3])]) {
 }
 
 h(event, context, (err, res)=>{
+    console.log("file loaded: " + eventFilepath);
     console.log("--- LAMBDA RESULTS");
     console.log(JSON.stringify(res, null, 2));
   }
